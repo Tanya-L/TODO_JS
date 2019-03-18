76f646f659413cc35b82c6b3e6ec76f1e14c765e
Tanya Ly <tanya.lytovchenko@gmail.com>
2019-03-18 18:13:55 +0100

    appending text to the TODO list, checking checkbox

--- todo.js
+++ todo.js
@@ -5,8 +5,30 @@ $(document).ready(function () {
         let item = $('#todo-list-item').val();
         event.preventDefault();
 
-        let newItem = $("<li></li>").text(item);
-        $('#todo-items').append(newItem);
+        if (item !== "") {
+            let newItem = $("<li></li>").text(item);
+            let newCheck = $('<input type="checkbox" class="todo-item checkbox"/>');
+            
+            //if the checkbox is Checked
+            newCheck.click(function () {
+                $(this).parent().toggleClass('completed');
+            })
+
+            let deleteLink = $('<a href="#" class="todo-item">x</a> <hr/>').click(function (event) {
+                newItem.remove();
+            });
+
+            // clear textbox
+            $("#todo-list-item").val("");
+
+            // add checkbox and delete link
+            newItem.prepend(newCheck).append(deleteLink);
+
+
+
+
+            $('#todo-items').append(newItem);
+        }
     });
 
 });
